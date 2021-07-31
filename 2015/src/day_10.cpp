e9e09c79128157d5a88f47ba71cbd2c1c65794e3
ajmorton <ajmorton2@gmail.com>
2021-08-01 09:38:12 +1000

    2015 - Speedup Day 10

--- 2015/src/day_10.cpp
+++ 2015/src/day_10.cpp
@@ -1,22 +1,38 @@
 #include "day_10.hpp"
 
-string lookAndSay(string line) {
-    std::stringstream sstream;
-    for(auto pos = line.begin(); pos != line.end(); ) {
-        auto nextPos = std::find_if(pos, line.end(), [pos](char c){return c != *pos;});
-        sstream << (nextPos - pos) << *pos;
-        pos = nextPos;
+int lookAndSay(vector<char>& word1, vector<char>& word2) {
+    int ptr1 = 0;
+    int ptr2 = 0;
+    char curNum = word1[0];
+    int curPos = 0;
+    while(word1[ptr1++] != 0) {
+        if(word1[ptr1] != curNum) {
+            word2[ptr2++] = ptr1 - curPos;
+            word2[ptr2++] = curNum;
+            curNum = word1[ptr1];
+            curPos = ptr1;
+        }
     }
-    return sstream.str();
+    word2[ptr2] = 0;
+
+    return ptr2;
 }
 
 tuple<int, int> day_10(string input) {
+    int lenNewWord = 0;
+    int p1 = 0;
 
-    int p1;
-    for(int i = 1; i <= 50; i++) {
-        input = lookAndSay(input);
-        if(i == 40) {p1 = input.length();}
+    vector<char> word1(4'000'000, 0);
+    vector<char> word2(4'000'000, 0);
+
+    int ptr = 0;
+    for(char c: input) { word1[ptr++] = c - '0'; }
+
+    for(int loop = 1; loop <= 50; loop++) {
+        lenNewWord = lookAndSay(word1, word2);
+        if(loop == 40) { p1 = lenNewWord; }
+        std::swap(word1, word2);
     }
 
-    return {p1, input.length()};
+    return {p1, lenNewWord};
 }
