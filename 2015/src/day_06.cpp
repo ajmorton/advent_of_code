c708bacd6bff39c0dc8a2fada95595bb48a4081a
ajmorton <ajmorton2@gmail.com>
2021-08-01 10:14:31 +1000

    2015 - Speedup Day 06

--- 2015/src/day_06.cpp
+++ 2015/src/day_06.cpp
@@ -10,22 +10,25 @@ instr_t parseInstr(string instr) {
     return instr_t{matches[1], stoi(matches[2]), stoi(matches[3]), stoi(matches[4]), stoi(matches[5])};
 }
 
+void turnOn( cell_t& cell){cell.fst  = 1, cell.snd += 1;                         return;};
+void turnOff(cell_t& cell){cell.fst  = 0, cell.snd  = std::max(0, cell.snd - 1); return;};
+void toggle( cell_t& cell){cell.fst ^= 1, cell.snd += 2;                         return;};
+
 tuple<int, int> day_06(string input) {
     vector<cell_t> grid(1000 * 1000, cell_t{0,0});
 
     for(string line: splitOn(input, '\n')) {
         auto [action, min_r, min_c, max_r, max_c] = parseInstr(line);
 
+        auto update = turnOn;
+        if(action == "turn on") { update = turnOn; } 
+        else if(action == "turn off"){ update = turnOff; }
+        else {update = toggle;}
+
         for(int r = min_r; r <= max_r; r++) {
             for(int c = min_c; c <= max_c; c++) {
                 auto& cell = grid[1000*r + c];
-                if(action == "turn on") {
-                    cell = {1, cell.snd + 1};
-                } else if (action == "toggle") {
-                    cell = {cell.fst ^ 1, cell.snd + 2};
-                } else if (action == "turn off") {
-                    cell = {0, std::max(cell.snd - 1, 0)};
-                }
+                update(cell);
             }
         }
     }
