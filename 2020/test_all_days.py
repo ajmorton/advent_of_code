27d7845bba229cd91f7ddef27aee7968a1906e47
ajmorton <ajmorton2@gmail.com>
2021-08-01 14:08:53 +1000

    2020 - Report execution times

--- 2020/test_all_days.py
+++ 2020/test_all_days.py
@@ -1,82 +1,71 @@
-import unittest
+from datetime import datetime
 import day_1,  day_2,  day_3,  day_4,  day_5,  day_6,  day_7,  day_8,  day_9,  day_10
 import day_11, day_12, day_13, day_14, day_15, day_16, day_17, day_18, day_19, day_20
 import day_21, day_22, day_23, day_24, day_25
 
-# Run with `python -m unittest`
+GREY = '\033[90m'
+GREEN = '\033[32m'
+RED = '\033[91m'
+END = '\033[0m'
 
-class Tests(unittest.TestCase):
-    def test_day_1(self):
-        self.assertEqual(day_1.run(), (436404, 274879808))
+def run_test(module, expected_output):
+    start = datetime.now()
+    result = module.run()
+    exec_time = (datetime.now() - start).total_seconds()
 
-    def test_day_2(self):
-        self.assertEqual(day_2.run(), (477, 686))
+    test_result = f"{GREEN} OK{END}"
+    if result != expected_output:
+        test_result = f"{RED}NOK{END}"
 
-    def test_day_3(self):
-        self.assertEqual(day_3.run(), (270, 2122848000))
+    if exec_time < 0.001:
+        (time, unit, color) = (exec_time * 1e6, "us", GREY)
+    elif exec_time < 1:
+        (time, unit, color) = (exec_time * 1e3, "ms", "")
+    else:
+        (time, unit, color) = (exec_time, "s", RED)
 
-    def test_day_4(self):
-        self.assertEqual(day_4.run(), (247, 145))
+    print(f"{module.__name__:6}  {test_result} {color} {round(time, 1): >5} {unit} {END}")
 
-    def test_day_5(self):
-        self.assertEqual(day_5.run(), (888, 522))
+    if result != expected_output:
+        offset = len(str(expected_output[0]))
+        print(f"    expected: {expected_output[0]: >{offset}}, {expected_output[1]}")
+        print(f"    actual:   {result[0]: >{offset}}, {result[1]}")
 
-    def test_day_6(self):
-        self.assertEqual(day_6.run(), (6742, 3447))
+    return exec_time
 
-    def test_day_7(self):
-        self.assertEqual(day_7.run(), (224, 1488))
 
-    def test_day_8(self):
-        self.assertEqual(day_8.run(), (1818, 631))
+if __name__ == "__main__":
 
-    def test_day_9(self):
-        self.assertEqual(day_9.run(), (20874512, 3012420))
+    tests = [
+        (day_1 , (436404,          274879808)),
+        (day_2 , (477,             686)),
+        (day_3 , (270,             2122848000)),
+        (day_4 , (247,             145)),
+        (day_5 , (888,             522)),
+        (day_6 , (6742,            3447)),
+        (day_7 , (224,             1488)),
+        (day_8 , (1818,            631)),
+        (day_9 , (20874512,        3012420)),
+        (day_10, (2232,            173625106649344)),
+        (day_11, (2093,            1862)),
+        (day_12, (759,             45763)),
+        (day_13, (370,             894954360381385)),
+        (day_14, (14839536808842,  4215284199669)),
+        (day_15, (289,             1505722)),
+        (day_16, (20091,           2325343130651)),
+        (day_17, (336,             2620)),
+        (day_18, (24650385570008,  158183007916215)),
+        (day_19, (203,             304)),
+        (day_20, (32287787075651,  1939)),
+        (day_21, (2436,            'dhfng,pgblcd,xhkdc,ghlzj,dstct,nqbnmzx,ntggc,znrzgs')),
+        (day_22, (34566,           31854)),
+        (day_23, (82635947,        157047826689)),
+        (day_24, (282,             3445)),
+        (day_25, (6421487,         False))
+    ]
 
-    def test_day_10(self):
-        self.assertEqual(day_10.run(), (2232, 173625106649344))
+    total_time = 0
+    for (day, expected_output) in tests:
+        total_time += run_test(day, expected_output)
 
-    def test_day_11(self):
-        self.assertEqual(day_11.run(), (2093, 1862))
-
-    def test_day_12(self):
-        self.assertEqual(day_12.run(), (759, 45763))
-
-    def test_day_13(self):
-        self.assertEqual(day_13.run(), (370, 894954360381385))
-
-    def test_day_14(self):
-        self.assertEqual(day_14.run(), (14839536808842, 4215284199669))
-
-    def test_day_15(self):
-        self.assertEqual(day_15.run(), (289, 1505722))
-
-    def test_day_16(self):
-        self.assertEqual(day_16.run(), (20091, 2325343130651))
-
-    def test_day_17(self):
-        self.assertEqual(day_17.run(), (336, 2620))
-
-    def test_day_18(self):
-        self.assertEqual(day_18.run(), (24650385570008, 158183007916215))
-
-    def test_day_19(self):
-        self.assertEqual(day_19.run(), (203, 304))
-
-    def test_day_20(self):
-        self.assertEqual(day_20.run(), (32287787075651, 1939))
-
-    def test_day_21(self):
-        self.assertEqual(day_21.run(), (2436, 'dhfng,pgblcd,xhkdc,ghlzj,dstct,nqbnmzx,ntggc,znrzgs'))
-
-    def test_day_22(self):
-        self.assertEqual(day_22.run(), (34566, 31854))
-
-    def test_day_23(self):
-        self.assertEqual(day_23.run(), (82635947, 157047826689))
-
-    def test_day_24(self):
-        self.assertEqual(day_24.run(), (282, 3445))
-
-    def test_day_25(self):
-        self.assertEqual(day_25.run(), (6421487, False))
+    print(f"\ntotal time = {round(total_time, 2)} s")
