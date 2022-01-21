7c5e56939033df2dbf9991d845e4c2ea75b87a0c
ajmorton <ajmorton2@gmail.com>
2022-01-21 18:37:48 +1100

    2021 - Day 02

---
+++ 2021/src/days/day02.zig
@@ -0,0 +1,56 @@
+const expect = @import("std").testing.expect;
+const std = @import("std");
+const stdout = std.io.getStdOut().writer();
+const Vector = std.meta.Vector;
+
+const helpers = @import("../helpers.zig");
+
+pub const RetDay2 = struct { p1: i32, p2: i32 };
+
+const Direction = enum { up, down, forward };
+const Move = struct { dir: Direction, dist: i32 };
+
+pub fn run(alloc: std.mem.Allocator) !RetDay2 {
+    const lines = try helpers.readInAs(alloc, "input/day02.txt", []u8);
+    defer lines.deinit();
+
+    return try solve(lines.items);
+}
+
+fn solve(lines: [][]const u8) !RetDay2 {
+    var vert: i32 = 0;
+    var vert2: i32 = 0;
+    var hor: i32 = 0;
+
+    for (lines) |line| {
+        var move = try parseMove(line);
+        switch (move.dir) {
+            Direction.up => vert += move.dist,
+            Direction.down => vert -= move.dist,
+            Direction.forward => {
+                hor += move.dist;
+                vert2 += move.dist * -vert;
+            },
+        }
+    }
+
+    return RetDay2{
+        .p1 = try std.math.absInt(vert * hor),
+        .p2 = try std.math.absInt(vert2 * hor),
+    };
+}
+
+fn parseMove(str: []const u8) !Move {
+    var split = std.mem.tokenize(u8, str, " ");
+    return Move{
+        .dir = std.meta.stringToEnum(Direction, split.next().?).?,
+        .dist = try std.fmt.parseInt(i32, split.rest(), 10),
+    };
+}
+
+test "Examples Day 2" {
+    var lines = [_][]const u8{ "forward 5", "down 5", "forward 8", "up 3", "down 8", "forward 2" };
+    var res = try solve(&lines);
+    try expect(res.p1 == 150);
+    try expect(res.p2 == 900);
+}
--- 2021/src/main.zig
+++ 2021/src/main.zig
@@ -3,12 +3,13 @@ const std = @import("std");
 const stdout = std.io.getStdOut().writer();
 
 const d1 = @import("days/day01.zig");
+const d2 = @import("days/day02.zig");
 
 var general_purpose_allocator = std.heap.GeneralPurposeAllocator(.{}){};
 const gpa = general_purpose_allocator.allocator();
 
 pub fn main() !void {
-    const result = d1.run(gpa);
+    const result = d2.run(gpa);
     try stdout.print("===== Results =====\n", .{});
     try stdout.print("{any}\n", .{result});
 }
@@ -17,4 +18,10 @@ test "Day 1" {
     var res_d1 = try d1.run(gpa);
     try expect(res_d1.p1 == 1715);
     try expect(res_d1.p2 == 1739);
+}
+
+test "Day 2" {
+    var res_d2 = try d2.run(gpa);
+    try expect(res_d2.p1 == 1868935);
+    try expect(res_d2.p2 == 1965970888);
 }
