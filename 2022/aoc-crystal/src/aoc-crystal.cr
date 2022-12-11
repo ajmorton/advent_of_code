ae6830f248ce1bf770506a8af821cea49d0c693f
ajmorton <ajmorton2@gmail.com>
2022-12-11 15:48:05 +1100

    2022 - Touch up main file

--- 2022/aoc-crystal/src/aoc-crystal.cr
+++ 2022/aoc-crystal/src/aoc-crystal.cr
@@ -6,12 +6,12 @@ require "./days/*"
 main()
 
 def main
-  day_to_run = "0"
+  day_to_run = nil
   benchmark = false
 
   OptionParser.parse do |parser|
     parser.banner = "Usage: aoc-crystal [arguments]"
-    parser.on("-d TEST", "The day to run (defaults to all)") { |_day| day_to_run = _day }
+    parser.on("-d TEST", "The day to run (defaults to all)") { |_day| day_to_run = _day.to_i }
     parser.on("-b", "Run in benchmark mode") { benchmark = true }
     parser.on("-h", "Show this help") { puts parser; exit }
     parser.invalid_option do |flag|
@@ -21,30 +21,25 @@ def main
     end
   end
 
-  run_day(day_to_run.to_i, benchmark)
+  run_day(day_to_run, benchmark)
 end
 
-def run_day(n : Number, benchmark : Bool)
-  case n
-  when  1 then run_day("Day01", ->{ Day01.run("./src/inputs/day01.txt") }, benchmark)
-  when  2 then run_day("Day02", ->{ Day02.run("./src/inputs/day02.txt") }, benchmark)
-  when  3 then run_day("Day03", ->{ Day03.run("./src/inputs/day03.txt") }, benchmark)
-  when  4 then run_day("Day04", ->{ Day04.run("./src/inputs/day04.txt") }, benchmark)
-  when  5 then run_day("Day05", ->{ Day05.run("./src/inputs/day05.txt") }, benchmark)
-  when  6 then run_day("Day06", ->{ Day06.run("./src/inputs/day06.txt") }, benchmark)
-  when  7 then run_day("Day07", ->{ Day07.run("./src/inputs/day07.txt") }, benchmark)
-  when  8 then run_day("Day08", ->{ Day08.run("./src/inputs/day08.txt") }, benchmark)
-  when  9 then run_day("Day09", ->{ Day09.run("./src/inputs/day09.txt") }, benchmark)
-  when 10 then run_day("Day10", ->{ Day10.run("./src/inputs/day10.txt") }, benchmark)
+def run_day(n : Number?, benchmark : Bool)
+  days = [Day01, Day02, Day03, Day04, Day05, Day06, Day07, Day08, Day09, Day10]
+
+  if n && days[n - 1]?
+    run_day(days[n - 1], benchmark)
   else
     puts "Running all days:"
-    (1..10).each { |n| run_day(n, benchmark) }
+    days.each { |day| run_day(day, benchmark) }
   end
 end
 
-def run_day(day_name : String, func : Proc, benchmark : Bool)
-  case benchmark
-  in true  then Benchmark.ips { |x| x.report(day_name) { func.call } }
-  in false then puts "#{day_name}: #{func.call}"
+def run_day(day : Class, benchmark : Bool)
+  func = ->{ day.run("./src/inputs/#{day.to_s.downcase}.txt") }
+  if benchmark
+    Benchmark.ips { |x| x.report(day.to_s) { func.call } }
+  else
+    puts "#{day}: #{func.call}"
   end
 end
--- 2022/aoc-crystal/src/days/common.cr
+++
@@ -1,4 +0,0 @@
-# Parse the input file as a list of ints
-def parse_int_list(file_path : String) : Array(Int64)
-  File.read_lines(file_path).map(&.to_i64)
-end
