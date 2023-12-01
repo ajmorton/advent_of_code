a921fbbacb11437bacecc8d91aa0fc76ebb6886d
ajmorton <ajmorton2@gmail.com>
2023-12-01 16:43:15 +1100

    2023 - Day 01

--- 2023-nim/src/days/day01.nim
+++ 2023-nim/src/days/day01.nim
@@ -1,24 +1,61 @@
-# FIXME - This is 2019 Day 01
+import strutils, sequtils, math
 
-import aoc_common
-import math, sequtils
+proc numFromStr(str: string): int = 
+    let nums = filter(str.toSeq(), isDigit)
+    return parseInt(nums[0] & nums[^1])
 
-proc calcFuelNeeded(n: int): int =
-    return (n div 3) - 2
+proc toNumbers(str: string): seq[int] = 
+    var nums: seq[int]
+    var localStr = str
 
-proc calcFuelNeededRecursive(n: int): int =
-    let fuelNeeded = calcFuelNeeded(n)
-    if fuelNeeded <= 0:
-        return 0
-    else:
-        return fuelNeeded + calcFuelNeededRecursive(fuelNeeded)
+    while localStr.len() > 0:
+        if localStr.startsWith("one"):
+            nums.add(1)
+        elif localStr.startsWith("two"):
+            nums.add(2)
+        elif localStr.startsWith("three"):
+            nums.add(3)
+        elif localStr.startsWith("four"):
+            nums.add(4)
+        elif localStr.startsWith("five"):
+            nums.add(5)
+        elif localStr.startsWith("six"):
+            nums.add(6)
+        elif localStr.startsWith("seven"):
+            nums.add(7)
+        elif localStr.startsWith("eight"):
+            nums.add(8)
+        elif localStr.startsWith("nine"):
+            nums.add(9)
+        elif localStr[0] == '1':
+            nums.add(1)
+        elif localStr[0] == '2':
+            nums.add(2)
+        elif localStr[0] == '3':
+            nums.add(3)
+        elif localStr[0] == '4':
+            nums.add(4)
+        elif localStr[0] == '5':
+            nums.add(5)
+        elif localStr[0] == '6':
+            nums.add(6)
+        elif localStr[0] == '7':
+            nums.add(7)
+        elif localStr[0] == '8':
+            nums.add(8)
+        elif localStr[0] == '9':
+            nums.add(9)
 
-proc part1*(weights: seq[int]): int =
-    return sum(weights.map(calcFuelNeeded))
+        localStr = localStr[1..^1]
 
-proc part2*(weights: seq[int]): int =
-    return sum(weights.map(calcFuelNeededRecursive))
+    return nums
+
+proc numOrNumStrFromStr(str: string): int = 
+    let nums = toNumbers(str)
+    return 10 * nums[0] + nums[^1]
 
 proc run*(inputFile: string): (int, int) =
-    let weights = inputAsInts(input_file)
-    return (part1(weights), part2(weights))
+    let lines = readFile(input_file).strip(leading = false).splitLines()
+    let nums = lines.map(numFromStr)
+
+    return (sum(nums), lines.map(numOrNumStrFromStr).sum)
--- 2023-nim/tests/test_all_days.nim
+++ 2023-nim/tests/test_all_days.nim
@@ -1,5 +1,4 @@
 import unittest
-import sequtils
 
 import days/day01
 import days/day02
@@ -7,17 +6,8 @@ import days/day03
 
 test "day01":
   let (part1, part2) = day01.run("./input/day01.txt")
-  check part1 == 3125750
-  check part2 == 4685788
-
-  check day01.part1(toSeq([12])) == 2
-  check day01.part1(toSeq([14])) == 2
-  check day01.part1(toSeq([1969])) == 654
-  check day01.part1(toSeq([100756])) == 33583
-
-  check day01.part2(toSeq([14])) == 2
-  check day01.part2(toSeq([1969])) == 966
-  check day01.part2(toSeq([100756])) == 50346
+  check part1 == 55538
+  check part2 == 54875
 
 test "day02":
   let (part1, part2) = day02.run("./input/day02.txt")
