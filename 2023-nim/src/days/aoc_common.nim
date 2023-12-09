9752207818b7a29cea5386349ef4000a451eed19
ajmorton <ajmorton2@gmail.com>
2023-12-09 11:34:59 +1100

    2023 - Remove unused aoc_common.nim

--- 2023-nim/src/days/aoc_common.nim
+++
@@ -1,8 +0,0 @@
-# Common functions to use across days.
-
-import sequtils, strutils
-
-# Helper funcs to parse and process the input
-proc inputAsInts*(input_file: string): seq[int] =
-    let input = readFile(input_file).strip(leading = false)
-    return input.splitLines.map(parseInt)
