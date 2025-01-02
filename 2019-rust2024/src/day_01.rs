d2ff69a7c58db9aef7cb273f8468f7a7571ac788
Andrew Morton <ajmorton2@gmail.com>
2025-01-02 23:14:14 +1100

    2024 - Day 01 - Clean up

--- 2019-rust2024/src/day_01.rs
+++ 2019-rust2024/src/day_01.rs
@@ -1,10 +1,9 @@
-
 fn fuel_cost(weight: isize) -> isize {
     weight / 3 - 2
 }
 
 fn fuel_cost_rec(weight: isize) -> isize {
-    let fuel_weight = (weight / 3) - 2;
+    let fuel_weight = weight / 3 - 2;
     if fuel_weight <= 0 {
         0
     } else {
@@ -14,23 +13,12 @@ fn fuel_cost_rec(weight: isize) -> isize {
 
 #[must_use]
 pub fn run() -> (isize, isize) {
-
-    let mut p1 = 0;
-    let mut p2 = 0;
-
-    let module_weights: Vec<isize> = include_str!("../input/day01.txt")
+    include_str!("../input/day01.txt")
         .lines()
-        .map(|x| x.parse().unwrap())
-        .collect();
-
-    for module in module_weights {
-        p1 += fuel_cost(module);
-        p2 += fuel_cost_rec(module);
-    }
-
-    (p1, p2)
+        .map(|x| x.parse::<isize>().unwrap())
+        .map(|w| (fuel_cost(w), fuel_cost_rec(w)))
+        .fold((0, 0), |l, r| (l.0 + r.0, l.1 + r.1))
 }
-
 
 #[test]
 fn day_01() {
