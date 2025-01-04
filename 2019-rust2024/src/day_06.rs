cc31cbe624390e0e18b6d1e771b575c4b17f0f04
Andrew Morton <ajmorton2@gmail.com>
2025-01-04 19:18:33 +1100

    2019 - Day 06 - Clean up

--- 2019-rust2024/src/day_06.rs
+++ 2019-rust2024/src/day_06.rs
@@ -1,56 +1,71 @@
-use ahash::{AHashMap, AHashSet};
+use ahash::AHashMap;
 
-type Orbits<'a> = AHashMap<&'a str, AHashSet<&'a str>>;
+type Orbits = Vec<usize>;
 
-fn num_orbiting(body: &str, orbits: &Orbits) -> usize {
-    let mut num_orbits = 0;
-    if let Some(parent) = orbits.get(body) {
-        num_orbits += parent.len();
-        for p in parent {
-            num_orbits += num_orbiting(p, &orbits);
-        }
+fn num_orbiting(body_id: usize, orbits_vec: &Orbits) -> usize {
+    let parent = orbits_vec[body_id];
+    if parent != 0 {
+        1 + num_orbiting(parent, orbits_vec)
+    } else {
+        0
     }
-    num_orbits
 }
 
-fn path_to_com<'a>(body: &'a str, orbits: &'a Orbits) -> AHashSet<&'a str> {
-
-    if body == "COM" {
-        return AHashSet::new();
+fn path_to_com(body: usize, orbits_vec: &Orbits) -> Vec<usize> {
+    let parent = orbits_vec[body];
+    if parent != 0 {
+        let mut path = path_to_com(parent, orbits_vec);
+        path.push(body);
+        path
     } else {
-        if let Some(parent) = orbits.get(body) {
-            let p = parent.iter().next().unwrap();
-            let mut path = path_to_com(p, orbits);
-            path.insert(body);
-            return path;
-        }
+        vec![0]
     }
-    unreachable!();
+}
+
+fn get_numeric_id<'a>(planet: &'a str, planet_ids: &mut AHashMap<&'a str, usize>) -> usize {
+    if planet_ids.contains_key(planet) {
+        planet_ids[planet]
+    } else {
+        let unique_id = planet_ids.len() + 1;
+        planet_ids.insert(planet, unique_id);
+        unique_id
+    }
 }
 
 #[must_use]
 pub fn run() -> (usize, usize) {
     let input = include_str!("../input/day06.txt").trim_ascii().lines();
-    let mut orbits: Orbits  = AHashMap::new();
+
+    let mut planet_id: AHashMap<&str, usize> = AHashMap::new();
+    let num_bodies = input.clone().count();
+    let mut orbit_vec: Orbits = vec![0; 2 * num_bodies];
 
     for line in input {
         let mut split = line.split(')');
         let (a, b) = (split.next().unwrap(), split.next().unwrap());
-        orbits.entry(b).or_default().insert(a);
+
+        let a_num = get_numeric_id(a, &mut planet_id);
+        let b_num = get_numeric_id(b, &mut planet_id);
+
+        orbit_vec[b_num] = a_num;
     }
 
-    let mut p1 = 0;
-    for body in orbits.keys() {
-        p1 += num_orbiting(body, &orbits);
-    }
+    let all_orbits: usize = (0..orbit_vec.len()).map(|b| num_orbiting(b, &orbit_vec)).sum();
 
-    let my_path = path_to_com("YOU", &orbits);
-    let san_path = path_to_com("SAN", &orbits);
+    let you_path = path_to_com(planet_id["YOU"], &orbit_vec);
+    let san_path = path_to_com(planet_id["SAN"], &orbit_vec);
 
-    let differing_planets = my_path.symmetric_difference(&san_path);
+    let common_path_len = you_path
+        .iter()
+        .zip(san_path.iter())
+        .take_while(|(x, y)| **x == **y)
+        .count();
 
-    // minus two because SAN and YOU are included
-    let num_hops = differing_planets.count() - 2;
+    // Removing the common path to COM yields the paths from YOU to COMMON_PARENT and SAN to COMMON_PARENT.
+    // By extension the sum of these paths is the path from YOU to SAN. 
+    // -1 as we want jumps between planets, not number of planets
+    let jumps_you_common = you_path[common_path_len..].len() - 1;
+    let jumps_common_san = san_path[common_path_len..].len() - 1;
 
-    (p1, num_hops)
+    (all_orbits, jumps_you_common + jumps_common_san)
 }
